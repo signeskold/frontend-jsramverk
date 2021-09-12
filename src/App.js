eea61520308a8238a175494c0cd85525d6818dc3
Signe <signe.skold@hotmail.com>
2021-09-12 21:37:18 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [editor, setEditor] = useState(null);
+    const data = "";
+    function alertData() {
+        alert(editor.getData());
+    }
+    return (
+        <div className="App">
+        <button onClick={alertData}>Spara</button>
+        <h2>Using CKEditor 5 in React</h2>
+        <CKEditor
+            editor={ ClassicEditor }
+            data="<p>Hello from CKEditor 5!</p>"
+            onReady={ editor => {
+                setEditor(editor);
+                console.log( 'Editor is ready to use!', editor );
+            } }
+            onChange={ ( event, editor ) => {
+                const data = editor.getData();
+                console.log( { event, editor, data } );
+            } }
+            onBlur={ ( event, editor ) => {
+                console.log( 'Blur.', editor );
+            } }
+            onFocus={ ( event, editor ) => {
+                console.log( 'Focus.', editor );
+            } }
+        />
+        </div>
+    );
 }
 
 export default App;
